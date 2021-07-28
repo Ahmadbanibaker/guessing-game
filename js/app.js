e1825a822513d2b3e88a5d4036434787836fc311
Ahmadbanibaker <ahmadmurad502@gmail.com>
2021-07-28 13:35:07 +0300

    added functions

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 'use strict';
+let correct = 0;
 let userwelcome = prompt ('what is your name :)');
 alert('I am Glad that you are here'+ userwelcome);
-
+ function knowme(){
 let knowme = prompt ('Do we know each other ?');
 switch(knowme.toLowerCase()) {
     case 'yes':
       alert ('Glad to hear about you ');
+      correct++;
      // console.log(knowme);
       break;
     case'no':
@@ -16,11 +18,14 @@ switch(knowme.toLowerCase()) {
       alert('You did not answer ');
       //console.log(knowme);
   }
-
+}
+knowme();
+function likemansaf(){
   let likemansaf = prompt ('Do you think I like mansaf? ');
 switch(likemansaf.toLowerCase()) {
     case 'yes':
       alert (' yes , who does not like it ^-^  ');
+      correct++;
      // console.log(like);
       break;
     case 'no':
@@ -31,10 +36,14 @@ switch(likemansaf.toLowerCase()) {
       alert('You did not answer :(');
       //console.log(like);
   }
+}
+  likemansaf();
+  function likemansaf(){
   let likefootball = prompt ('Do you think I watch football ?  ');
   switch(likefootball.toLowerCase()) {
       case 'yes':
         alert ('That is right , actually I am a big fan ');
+        correct++;
        // console.log(learn);
         break;
       case 'no':
@@ -45,10 +54,14 @@ switch(likemansaf.toLowerCase()) {
         alert('You did not answer ');
         //console.log(learn);
     }
+  }
+  likemansaf();
+  function likewinter(){
     let likewinter =prompt ('Do you think I like winter season ? ');
     switch(likewinter.toLowerCase()) {
         case 'yes':
           alert ('yes I do , it is much better than summer for me :) ');
+          correct++;
          // console.log(thing);
           break;
         case 'no':
@@ -59,10 +72,14 @@ switch(likemansaf.toLowerCase()) {
           alert('You did not answer ');
           //console.log(thing);
       }
+    }
+    likewinter();
+    function likemovies(){
       let likemovies = prompt('Do you thinck I like movies or not ?  ');
     switch(likemovies.toLowerCase()) {
         case 'yes':
           alert (' yes , I like them ^-^  ');
+          correct++;
          // console.log(last);
           break;
         case 'no':
@@ -73,11 +90,16 @@ switch(likemansaf.toLowerCase()) {
           alert('You did not answer : (');
           //console.log(last); 
       }
+    }
+    likemovies();
+
       alert('Thanks for your answers :) '+ userwelcome); 
-      function question6(){
+
+      function guessnumber(){
       let guess = prompt('Guess a number between 1 and 10');
       let numGuess = Number(guess);
       let answer = 7;
+      
 
       for (let i = 0; i < 4; i++ ){
         if (answer === numGuess){
@@ -91,15 +113,15 @@ switch(likemansaf.toLowerCase()) {
       }
         alert('The correct answer is 7')
     }
-    question6(); 
+    guessnumber(); 
       
       
-function question7(){
-  let favfood = ['Mansaf', 'Sushi', 'shawarma', 'Dolma', 'Pizza','Pasta']
+function favfood(){
+  let favfood = ['mansaf', 'mushi', 'shawarma', 'dolma', 'pizza','pasta']
 
   for(let y = 0; y < 6; y++){
     let foodGuess = prompt ('Can you guess my favorite food ?');
-    if (foodGuess.toLowerCase){
+    if (favfood.includes( foodGuess)){
       alert('You are correct ');
       correct++;
       break;
@@ -108,6 +130,6 @@ function question7(){
     }
   }
 }
-question7();
+favfood();
 
 alert('You got ' + correct + '/7');
